01e1e642a16677b368c885275e125063cd679899
OLUWATIMILEHIN BANJO <TIMMYBANJO@GMAIL.COM>
2022-12-27 12:27:50 +0100

    focus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ({searchTerm,setSearchTerm,user}) => {
   return (
     <div className='flex gap-2 md:gap-5 w-full mt-5 pd-7'>
       {/* search input area */}
-      <div className='flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focul-within:shadow-sm '>
+      <div className='flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm '>
         <IoMdSearch fontSize={21} className='ml-1'/>
         <input
         type='text'
